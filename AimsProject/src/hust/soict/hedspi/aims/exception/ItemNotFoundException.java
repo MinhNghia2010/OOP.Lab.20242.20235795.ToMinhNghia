1a0b4d4fa53669904b62165351d6e67880f5cf7f
MinhNghia2010 <minhnghia2010200500@gmail.com>
2025-06-01 21:19:22 +0700

    Delete AimsProject directory

--- AimsProject/src/hust/soict/hedspi/aims/exception/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.hedspi.aims.exception;
-
-public class ItemNotFoundException extends Exception {
-    public ItemNotFoundException(String message) {
-        super(message);
-    }
-}
--- AimsProject/src/hust/soict/hedspi/aims/exception/NegativePriceException.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.hedspi.aims.exception;
-
-public class NegativePriceException extends Exception {
-    public NegativePriceException(String message) {
-        super(message);
-    }
-}
